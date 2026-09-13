3288c89af9e06faa65122309e4a3eaa2e2db713b
yuki-devlab <yuki61051108@gmail.com>
2026-09-14 05:47:02 +0900

    feat: add auth storage

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,35 @@
+import { drizzleAdapter } from "@better-auth/drizzle-adapter";
 import { betterAuth } from "better-auth";
+import { db } from "@/db";
+import * as schema from "@/db/schema";
+import { redisSecondaryStorage } from "@/lib/redis-secondary-storage";
 
 export const auth = betterAuth({
 	baseURL: process.env.BETTER_AUTH_URL,
+	database: drizzleAdapter(db, {
+		provider: "sqlite",
+		schema,
+	}),
+	rateLimit: {
+		storage: "secondary-storage",
+	},
+	secondaryStorage: redisSecondaryStorage,
+	session: {
+		storeSessionInDatabase: true,
+		expiresIn: 60 * 60 * 24 * 7,
+		updateAge: 60 * 60 * 24,
+		cookieCache: {
+			enabled: true,
+			maxAge: 60 * 5,
+		},
+	},
 	socialProviders: {
 		google: {
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		},
+	},
+	verification: {
+		storeInDatabase: true,
 	},
 });
