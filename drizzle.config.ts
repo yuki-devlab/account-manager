67b19d268034baa458f547cf1589bccd1776e522
yuki-devlab <yuki61051108@gmail.com>
2026-09-14 03:18:29 +0900

    chore: load .env.local

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,7 @@
-import "dotenv/config";
+import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
+
+config({ path: ".env.local" });
 
 const url = process.env.TURSO_DATABASE_URL;
 const authToken = process.env.TURSO_AUTH_TOKEN;
