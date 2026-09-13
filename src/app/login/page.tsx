d6e2620da4154e82189c2f6673412865bdbbc32a
yuki-devlab <yuki61051108@gmail.com>
2026-09-13 22:56:09 +0900

    feat: add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginButton from "@/app/login/_components/LoginButton";
+
+export default function LoginPage() {
+	return (
+		<div className="flex h-screen items-center justify-center">
+			<LoginButton />
+		</div>
+	);
+}
